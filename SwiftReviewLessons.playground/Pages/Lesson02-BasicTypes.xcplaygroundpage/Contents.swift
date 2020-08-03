6434678d8fb4676a16442b3729a6088932992471
Yuliia Engman <yuliia.engman@gmail.com>
2020-08-03 00:38:38 -0400

    lesson two

---
+++ SwiftReviewLessons.playground/Pages/Lesson02-BasicTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let number = 25 // equal to let number: Int = 25 -> prints 25
+print(number)
+
+let anotherNumber: Double = 35 // prints -> 35.0
+
+print(anotherNumber)
+
+// for big numbers we can use underscore _
+let bigNumber = 1_000_000 // prints 1000000
+print(bigNumber)
