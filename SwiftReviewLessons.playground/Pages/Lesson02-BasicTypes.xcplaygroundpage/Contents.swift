7127552952d7df9c0477d00d20757b0ea2959252
Yuliia Engman <yuliia.engman@gmail.com>
2020-08-04 23:37:46 -0400

    lesson on basic types

--- SwiftReviewLessons.playground/Pages/Lesson02-BasicTypes.xcplaygroundpage/Contents.swift
+++ SwiftReviewLessons.playground/Pages/Lesson02-BasicTypes.xcplaygroundpage/Contents.swift
@@ -10,3 +10,21 @@ print(anotherNumber)
 // for big numbers we can use underscore _
 let bigNumber = 1_000_000 // prints 1000000
 print(bigNumber)
+
+// Float = 0,000000 (6 разрядов посде запятой)
+
+// Double = 0,000000000000000 (15 разрядов посде запятой)
+
+let a = 5
+let b = 5
+
+let c = a + b
+
+//BUT
+let d = 5 // Int
+let e = 5.0 // Double
+
+// let f = d + e // Cannot add Int + Double
+//We can ->
+let f = d + Int(e) //   Но оно не округляет, а обрезает!
+// Но лучше переводить в дабл чтоб результвт был самым точным
