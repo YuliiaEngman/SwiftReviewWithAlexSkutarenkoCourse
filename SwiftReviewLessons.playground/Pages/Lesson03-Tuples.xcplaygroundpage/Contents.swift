087745b16391e269d625c312cffb8ae4d90381c7
Yuliia Engman <yuliia.engman@gmail.com>
2020-08-05 23:34:59 -0400

    homework on tuples

--- SwiftReviewLessons.playground/Pages/Lesson03-Tuples.xcplaygroundpage/Contents.swift
+++ SwiftReviewLessons.playground/Pages/Lesson03-Tuples.xcplaygroundpage/Contents.swift
@@ -38,6 +38,7 @@ print(check2)
 // Создаются так же чтоб назвать значения
 
 let tuple = (index: 1, phrase: "Hello", registered: true, latency: 2.4)
+print(tuple)
 print(tuple.0)
 print(tuple.1)
 print(tuple.2)
@@ -88,4 +89,45 @@ print((totalNumber, merchantName))
 
 
 
-// HOMDE WORK
+// HOME WORK
+
+var tupleHome = (pushups: 30, pullups: 40, situps: 50)
+
+print(tupleHome)
+
+print("I can make \(tupleHome.pushups) pushups.")
+print("I can make \(tupleHome.pullups) pullups.")
+print("I can make \(tupleHome.situps) situps.")
+
+print("I can't make more than \(tupleHome.0) pushups.")
+print("I can't make more than \(tupleHome.1) pullups.")
+print("I can't make more than \(tupleHome.2) situps.")
+
+
+let husTuple = (pushups: 130, pullups: 140, situps: 150)
+
+print(husTuple)
+
+print("Hun can make \(husTuple.pushups) pushups.")
+print("Hun can make \(husTuple.pullups) pullups.")
+print("Hun can make \(husTuple.situps) situps.")
+
+print("Hun can't make more than \(husTuple.0) pushups.")
+print("Hun can't make more than \(husTuple.1) pullups.")
+print("Hun can't make more than \(husTuple.2) situps.")
+
+let pushupsDifference = (husTuple.pushups) - (tupleHome.pushups)
+let pullupsDifference = (husTuple.1) - (tupleHome.1)
+let situpsDifference = (husTuple.2) - (tupleHome.situps)
+
+print("My Hum cam make on \(pushupsDifference) than I can.")
+print("My Hum cam make on \(pullupsDifference) than I can.")
+print("My Hum cam make on \(situpsDifference) than I can.")
+
+let tempPushups = husTuple.0
+let tempPullups = husTuple.1
+let tempSitups = husTuple.1
+
+tupleHome = (tempPushups, tempPullups, tempSitups)
+
+
